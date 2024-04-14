712e504a3dae8254fc9894ed2ed7271ef39e3954
Questionrat <140145679+PiecesAB@users.noreply.github.com>
2024-04-13 20:52:28 -0700

    prevent death animation from replay desync

---
+++ LowLevel/AnimationFrameAdvance.cs
@@ -0,0 +1,30 @@
+using Blastula.VirtualVariables;
+using Godot;
+
+namespace Blastula
+{
+    /// <summary>
+    /// This class ensures the animation is NOT affected by framerate,
+    /// but instead advances a certain amount every frame.
+    /// </summary>
+    /// <remarks>
+    /// We require this to ensure replays are deterministic.
+    /// </remarks>
+    public partial class AnimationFrameAdvance : AnimationPlayer
+    {
+        public override void _Ready()
+        {
+            base._Ready();
+            Pause();
+        }
+
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+            Pause();
+            double animationStep = Engine.TimeScale / Persistent.SIMULATED_FPS;
+            Advance(animationStep);
+        }
+    }
+}
+
